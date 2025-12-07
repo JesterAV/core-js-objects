9199ed80888c1ebaf4953c8690114f7483e89da9
Aleksei Bulash <winchesrers@gmail.com>
2025-12-07 18:19:15 +0300

    feat: implement sellTickets function

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -160,8 +160,28 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  let cash25 = 0;
+  let cash50 = 0;
+
+  for (let i = 0; i < queue.length; i += 1) {
+    if (queue[i] === 25) {
+      cash25 += 1;
+    } else if (queue[i] === 50) {
+      if (cash25 === 0) return false;
+    } else if (queue[i] === 100) {
+      if (cash25 >= 1 && cash50 >= 1) {
+        cash25 -= 1;
+        cash50 -= 1;
+      } else if (cash25 >= 3) {
+        cash25 += 3;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return true;
 }
 
 /**
