4cfb7c45317edfb1c15eeb46f7c99c5276f4f99b
Aleksei Bulash <winchesrers@gmail.com>
2025-12-07 18:01:15 +0300

    feat: implement makeWord function

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -130,8 +130,20 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const positions = Object.values(lettersObject).flat();
+
+  if (positions.length === 0) return '';
+
+  const arr = Array(Math.max(...positions) + 1);
+
+  Object.entries(lettersObject).forEach(([letter, position]) => {
+    position.forEach((pos) => {
+      arr[pos] = letter;
+    });
+  });
+
+  return arr.join('');
 }
 
 /**
